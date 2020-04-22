3aa9f98f14860e40e5d5e875f03166ed8fc76a32
wuli2496 <wuli2496@163.com>
2020-04-22 23:32:20 +0800

    添加拓扑排序

---
+++ LeetCode/Course Schedule II/Course Schedule II(拓扑排序bfs).java
@@ -0,0 +1,82 @@
+/**
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+Example 2:
+
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution 
+{
+    public int[] findOrder(int numCourses, int[][] prerequisites) 
+    {
+    	int[] inorders = new int[numCourses];
+    	
+    	List<List<Integer>> adjList = new ArrayList<>();
+    	for (int i = 0; i < numCourses; ++i)
+    	{
+    		adjList.add(new ArrayList<>());
+    	}
+    	
+    	for (int[] prerequisite : prerequisites)
+    	{
+    		int u = prerequisite[1];
+    		int v = prerequisite[0];
+    		adjList.get(u).add(v);
+    		++inorders[v];
+    	}
+    	
+    	Queue<Integer> queue = new LinkedList<>();
+    	for (int i = 0; i < numCourses; ++i)
+    	{
+    		if (inorders[i] == 0)
+    		{
+    			queue.add(i);
+    		}
+    	}
+    	
+    	int[] ans = new int[numCourses];
+    	int ansCnt = 0;
+    	while (!queue.isEmpty())
+    	{
+    		int u = queue.poll();
+    		ans[ansCnt] = u;
+    		++ansCnt;
+    		for (int v : adjList.get(u))
+    		{
+    			--inorders[v];
+    			if (inorders[v] == 0)
+    			{
+    				queue.add(v);
+    			}
+    		}
+    	}
+    	
+    	if (ansCnt != numCourses)
+    	{
+    		return new int[] {};
+    	}
+    	
+    	return ans;
+    }
+}
