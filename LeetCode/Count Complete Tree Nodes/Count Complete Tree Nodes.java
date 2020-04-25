89ae0826d5fd027da99c9769dd3cb217d8270e59
wuli2496 <wuli2496@163.com>
2020-04-25 16:33:49 +0800

    添加递归遍历获取完全二叉树结点数

---
+++ LeetCode/Count Complete Tree Nodes/Count Complete Tree Nodes.java
@@ -0,0 +1,38 @@
+
+/**
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input: 
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+ */
+
+
+
+class TreeNode 
+{
+	  int val;
+	  TreeNode left;
+	  TreeNode right;
+	  TreeNode(int x) { val = x; }
+}
+
+class Solution 
+{
+	public int countNodes(TreeNode root) 
+	{
+		return root == null ? 0 : 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
