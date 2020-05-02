436d9752f49a28d7a8e14b89215213f58e53281c
wuli2496 <wuli2496@163.com>
2020-05-02 11:38:33 +0800

    添加二分法

---
+++ LeetCode/H-Index II/H-Index II(二分).java
@@ -0,0 +1,29 @@
+class Solution 
+{	
+	public int hIndex(int[] citations) 
+	{
+		int left = 0;
+		int n = citations.length;
+		int right = n - 1;
+		
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (citations[mid] == n - mid)
+			{
+				return n - mid;
+			}
+			else if (citations[mid] < n - mid)
+			{
+				left = mid + 1;
+			}
+			else 
+			{
+				right = mid - 1;
+			}
+		}
+		
+		return n - left;
+    }
+}
+
