6d31d8465066f2c25f6425c2e305a6c24b85d68b
wuli2496 <wuli2496@163.com>
2020-05-05 18:59:14 +0800

    Counting Bits(动态规划＋最高有效位).java

---
+++ LeetCode/Counting Bits/Counting Bits(动态规划＋最高有效位).java
@@ -0,0 +1,27 @@
+
+/*
+ * dp(x + b) = dp(x) + 1,其中b=2^m
+ * */
+
+class Solution 
+{
+	public int[] countBits(int num) 
+	{
+		int[] cnt = new int[num + 1];
+		int i = 0;
+		int b = 1;
+		
+		while (b <= num)
+		{
+			while (i < b && i + b <= num)
+			{
+				cnt[i + b] = cnt[i] + 1;  
+				++i;
+			}
+			i = 0;
+			b <<= 1;
+		}
+		
+		return cnt;
+    }
+}
