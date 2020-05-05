b652310cf36664dc7b80857e3d0b2d0f1f49bd04
wuli2496 <wuli2496@163.com>
2020-05-05 18:01:06 +0800

    House Robber III(树上最优子结构递归解法).java

---
+++ LeetCode/House Robber III/House Robber III(树上最优子结构递归解法).java
@@ -0,0 +1,34 @@
+
+class TreeNode 
+{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
+
+class Solution 
+{
+	public int rob(TreeNode root) 
+	{
+		if (root == null)
+		{
+			return 0;
+		}
+		
+		int money = root.val;
+		if (root.left != null)
+		{
+			money += rob(root.left.left) + rob(root.left.right);
+		}
+		
+		if (root.right != null)
+		{
+			money += rob(root.right.left) + rob(root.right.right);
+		}
+		
+		return Math.max(money, rob(root.left) + rob(root.right));
+    }
+	
+	
+}
