37936075c6c0d0efd9e29a98dea03be30afc7b8a
wuli2496 <wuli2496@163.com>
2020-05-22 21:40:45 +0800

    栈解决链表加法

---
+++ LeetCode/Add Two Numbers II/Add Two Numbers II.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode ans = null;
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+            int a = s1.isEmpty() ? 0 : s1.pop();
+            int b = s2.isEmpty() ? 0 : s2.pop();
+
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur %= 10;
+
+            ListNode node = new ListNode(cur);
+            node.next = ans;
+            ans = node;
+        }
+
+        return ans;
+    }
+}
+
