7d5899047d2bf0c6c0c8bd753348d06be6dcabca
wuli2496 <wuli2496@163.com>
2020-05-31 06:54:10 +0800

    添加codeforces模板

---
+++ codeforces/codeforcesTemplate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    ifstream fin("/home/wl/OJ/OJ/codeforces/codeforces_in");
+    streambuf* cinback = cin.rdbuf(fin.rdbuf());
+#endif
+
+#ifndef ONLINE_JUDGE
+    cin.rdbuf(cinback);
+#endif
+}
