00c2c6645e56cadb62027b14263e49675519ff44
wuli2496 <wuli2496@163.com>
2020-06-01 23:30:07 +0800

    修改模板添加宏及一些模板函数

--- codeforces/codeforcesTemplate.cpp
+++ codeforces/codeforcesTemplate.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+#define forn(i, s, e) for(int i = s; i < e; ++i)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 
+template <typename T> 
+istream& operator>>(istream& in, vector<T>& v) 
+{
+    for (auto& i : v) {
+        in >> i;
+    }
+
+    return in;
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, vector<T>& v)
+{
+    for (auto& i : v) {
+        out << i << " ";
+    }
+    out << endl;
+
+    return out;
+}
+
+template <typename T, typename U>
+void chkMin(T& a, U b)
+{
+    if (a > b) {
+        a = b;
+    }    
+}
+
+template <typename T, typename U>
+void chkMax(T& a, U b)
+{
+    if (a < b) {
+        a = b;
+    }
+}
 int main()
 {
     ios::sync_with_stdio(false);
