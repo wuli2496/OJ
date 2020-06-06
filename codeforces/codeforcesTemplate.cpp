3f81a49655c6119c4d010f37ef8e6274703c52da
wuli2496 <wuli2496@163.com>
2020-06-06 16:45:35 +0800

    添加输入输出优化

--- codeforces/codeforcesTemplate.cpp
+++ codeforces/codeforcesTemplate.cpp
@@ -49,6 +49,43 @@ void chkMax(T& a, U b)
     }
 }
 
+template <typename T> 
+inline void read(T& a)
+{
+    char c = getchar();
+    int f = 1;
+    int a = 0;
+    while (c > '9' || c < '0') {
+        if (c == '-') {
+            f = -1;
+        }
+
+        c = getchar();
+    }
+
+    while (c <= '9' && c >= '0') {
+        a = (a << 1) + (a << 3) + c - '0';
+        c = getchar();
+    }
+
+    a *= f;
+}
+
+template <typename T>
+void write(T a)
+{
+    if (a < 0) {
+        putchar('-');
+        a = -a;
+    }
+
+    if (a > 9) {
+        write(a / 10);
+    }
+
+    putchar(a % 10 + '0');
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
