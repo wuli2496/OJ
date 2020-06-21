c1cf609d693d8ffe9b790bf498a32725975f12da
wuli2496 <wuli2496@163.com>
2020-06-21 12:03:32 +0800

    动态规划中添加剪枝

---
+++ LeetCode/Partition Equal Subset Sum/Partition Equal Subset Sum(剪枝).java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int len = nums.length;
+        int target = sum / 2;
+        boolean[][] dp = new boolean[len][target + 1];
+
+        dp[0][0] = true;
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (nums[i] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+
+            if (dp[i][target]) {
+                return true;
+            }
+        }
+
+        return dp[len - 1][target];
+    }
+}
+
