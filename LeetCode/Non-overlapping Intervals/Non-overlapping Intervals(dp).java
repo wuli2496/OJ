30612c328c2891639c77c62072900f04a3130bbc
wuli2496 <wuli2496@163.com>
2020-08-08 16:16:56 +0800

    添加dp解法，基于起点从小到大排序

---
+++ LeetCode/Non-overlapping Intervals/Non-overlapping Intervals(dp).java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+    	if (intervals.length == 0) {
+    		return 0;
+    	}
+    	
+    	Arrays.sort(intervals, new IntervalComparator());
+    	int[] dp = new int[intervals.length];
+    	dp[0] = 1;
+    	int ans = 1;
+    	for (int i = 1; i < intervals.length; ++i) {
+    		int max = 0;
+    		for (int j = 0; j < i; ++j) {
+    			if (!isOverlapping(intervals[j], intervals[i])) {
+    				max = Math.max(max, dp[j]);
+    			}
+    		}
+    		
+    		dp[i] = max + 1;
+    		ans = Math.max(dp[i], ans);
+    	}
+    	
+    	return intervals.length - ans;
+    }
+    
+    private boolean isOverlapping(int[] interval1, int[] interval2) {
+    	return interval1[1] > interval2[0];
+    }
+    
+    private class IntervalComparator implements Comparator<int[]> {
+
+		@Override
+		public int compare(int[] arg0, int[] arg1) {
+			return arg0[0] - arg1[0];
+		}
+    	
+    }
+}
