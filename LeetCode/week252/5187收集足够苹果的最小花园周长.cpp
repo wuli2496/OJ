6cdf68f37b6178d5f9a3ff3a2ca3b915ad5aeabc
wuli2496 <wuli2496@163.com>
2021-08-01 17:07:10 +0800

    添加二分法

--- LeetCode/week252/5187收集足够苹果的最小花园周长.cpp
+++ LeetCode/week252/5187收集足够苹果的最小花园周长.cpp
@@ -7,3 +7,25 @@ public:
         return n * 8;
     }
 };
+
+/**
+ *添加二分法
+ */
+
+class Solution {
+public:
+    long long minimumPerimeter(long long neededApples) {
+        long long left = 1, right = 100000, ans;
+        while (left <= right) {
+            long long mid = (left + right) / 2;
+            if (2 * mid * (mid + 1) * (2 * mid + 1) >= neededApples) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans * 8;
+    }
+};
